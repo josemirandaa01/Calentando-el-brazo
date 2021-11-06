f198493817188408e8267dbc4394663726112833
josemirandaa01 <76072842+josemirandaa01@users.noreply.github.com>
2021-11-06 15:34:50 -0400

    Add files via upload

---
+++ Persistencia Multiplicativa.cs
@@ -0,0 +1,32 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+    int c,i = 0;
+    
+
+    Console.WriteLine("Inserte un número");
+    c = Convert.ToInt32(Console.ReadLine());
+    
+    while(c > 9)
+ {
+   int temp1 = 0;
+    
+    while(c != 0)
+    {
+     int temp2 = c % 10;
+     c = c - temp2;
+     c = c/10;
+     temp1 = c * temp2;
+     c = temp1;
+     i++;
+    }
+    
+    
+    
+    }
+	 Console.WriteLine("La persistencia es" + " " + i);
+	}
+}
